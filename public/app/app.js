9f3f10e8890c3022f9513d42408af7d378985be7
juan moreno <juan.moreno@moelia.com>
2015-10-28 10:48:19 +0100

    Added ui-router

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,27 @@
 (function() {
     "use strict";
 
-    angular.module('map-chat', ['uiGmapgoogle-maps', "geolocation"])
+    angular.module('map-chat', ['uiGmapgoogle-maps', "geolocation", "ui.router"])
         .config(['uiGmapGoogleMapApiProvider', function(GoogleMapApiProviders) {
             GoogleMapApiProviders.configure({
-                china: true
-            });
-        }]
-    );
+                    china: true
+                });
+            }]
+        )
+        .config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
+
+            $urlRouterProvider.otherwise("/home");
+
+            $stateProvider
+                .state("login", {
+                    url: "/login",
+                    templateUrl: "app/pages/login/login_template.html"
+                })
+                .state("home", {
+                    url: "/home",
+                    templateUrl: "app/pages/home/home_template.html",
+                    controller: "HomeController as home"
+                })
+        }]);
 
 })();
