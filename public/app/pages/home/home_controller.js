81853342e335f6686d170630a2cf3deb24f65c7e
juan moreno <juan.moreno@moelia.com>
2015-10-28 09:41:36 +0100

    google map marker first approach

--- public/app/pages/home/home_controller.js
+++ public/app/pages/home/home_controller.js
@@ -8,43 +8,62 @@
 
     function HomeController(SocketFactory, geolocation) {
         var home = this;
+        var randomValue = Math.floor(Math.random() * 100);
         home.user = {
             location: {
+                id: randomValue,
+                title: "m" + randomValue,
                 latitude: 0,
                 longitude: 0
             }
         };
-        home.map = {};
+        home.map = {
+            center: {
+                latitude: 38,
+                longitude: -1.9
+            },
+            zoom: 8
+        };
         home.messageText = "";
         home.messagesList = [];
+        home.usersLocationsList = [];
 
         home.sendMessage = sendMessage;
 
         activate();
 
         function activate() {
-            home.map = {
-                center: {
-                    latitude: 45,
-                    longitude: -73
-                },
-                zoom: 8
-            };
+            getGeolocation();
+            handleMessages();
+            handleUsers();
+        }
 
-            geolocation.getLocation().then(function(data) {
-                if (data) {
-                    home.user.location.latitude = data.coords.latitude;
-                    home.user.location.longitude = data.coords.longitude;
-                    SocketFactory.emit("userConnected", home.user);
-                }
-            });
-
+        function handleMessages() {
             SocketFactory.on("messages", function(message) {
                 console.log(message);
                 if (home.messagesList.length >= 9) {
                     home.messagesList.shift();
                 }
                 home.messagesList.push(message);
+            });
+        }
+
+        function handleUsers() {
+            SocketFactory.on("users", function(user) {
+                if (user) {
+                    console.log(user.location);
+                    home.usersLocationsList.push(user.location);
+                }
+            });
+        }
+
+        function getGeolocation() {
+            geolocation.getLocation().then(function(data) {
+                if (data) {
+                    home.user.location.latitude = data.coords.latitude;
+                    home.user.location.longitude = data.coords.longitude;
+                    SocketFactory.emit("userConnected", home.user);
+                }
             });
         }
 
