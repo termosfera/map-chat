b181987278f0f2154a74e97838efdebe4fd0fcb4
juan.moreno <juan.moreno@moelia.com>
2016-01-26 22:07:41 +0100

    final test

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var app             = express();
 
 // Logging and Parsing
 app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
-app.use('/public/bower_components',
-    express.static(__dirname + '/public/bower_components'));           // Use BowerComponents
+app.use('/bower_components',
+    express.static(__dirname + '/bower_components'));           // Use BowerComponents
 app.use(morgan('dev'));                                         // log with Morgan
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
